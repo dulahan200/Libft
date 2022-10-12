1ad946daaaa37a0dea94d1bebd56d538d46b1671
Hector Mestre <hmestre-@cbr12s1.42barcelona.com>
2022-10-12 18:14:42 +0200

    ready to deploy

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: hmestre- <hmestre-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 18:27:36 by hmestre-          #+#    #+#             */
-/*   Updated: 2022/10/10 20:04:03 by hmestre-         ###   ########.fr       */
+/*   Updated: 2022/10/12 16:32:59 by hmestre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -36,8 +36,6 @@ char	*ft_itoa(int n)
 
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
-//	if (n == INT_MIN)
-//		return (ft_strdup((char *)INT_MIN));
 	sign = 0;
 	if (n < 0)
 	{
@@ -51,10 +49,41 @@ char	*ft_itoa(int n)
 	nbr_string[digits_n - sign] = '\0';
 	while (digits_n-- - sign)
 	{
-		nbr_string[digits_n - sign] = (char)((n % 10) + '0'); 
+		nbr_string[digits_n - sign] = (char)((n % 10) + '0');
 		n = n / 10;
 	}
 	if (sign == -1)
 		nbr_string[0] = '-';
 	return (nbr_string);
 }
+/*
+char	*ft_itoa(int n)
+{
+	char	*nbr_string;
+	size_t	digits_n;
+	int		sign;
+
+//	if (n == -2147483648)
+//		return (ft_strdup("-2147483648"));
+	if (n == INT_MIN)
+		return (ft_strdup((char *)INT_MIN));
+	sign = 0;
+	if (n < 0)
+	{
+		sign = -1;
+		n = -n;
+	}
+	digits_n = ft_count_digits(n);
+	nbr_string = malloc(sizeof(char) * (digits_n + 1 - sign));
+	if (!nbr_string)
+		return (NULL);
+	nbr_string[digits_n - sign] = '\0';
+	while (digits_n-- - sign)
+	{
+		nbr_string[digits_n - sign] = (char)((n % 10) + '0');
+		n = n / 10;
+	}
+	if (sign == -1)
+		nbr_string[0] = '-';
+	return (nbr_string);
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hmestre- <hmestre-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 19:24:33 by hmestre-          #+#    #+#             */
-/*   Updated: 2022/10/10 20:09:01 by hmestre-         ###   ########.fr       */
+/*   Updated: 2022/10/12 16:25:56 by hmestre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,7 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
-void	 ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
